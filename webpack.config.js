74a1e8418b9019793722a6103c7c459412837721
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-02 11:30:09 -0500

    2.2 - Instalación de React y Babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,22 @@ module.exports = {
   },
 	// Plugins que vamos a utilizar y siempre va en array
 	plugins: [
-		new HtmlWebpackPlugin() // Asi utilizarmos la instacia del plugin
-	]
+		new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }) // Asi utilizarmos la instacia del plugin
+	],
+  module: {
+    rules: [ // reglas que tendran los modulos
+      {
+        test: /\.js$/, // utilizar esta regla para todos los archivo de js
+        exclude: /node_modules/, // No hacer estas transformaciones en las dependencias
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 }
