3e9a87ce9c4abacfcea9fb3444da8db0fcc05c0f
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-07 11:08:00 -0500

    4.2 - useEffect: limpiando eventos

--- src/Components/ListOfCategories/index.js
+++ src/Components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]) // array vacio -> ya damos por hecho que es un array
+  const [showFixed, setShowFixed] = useState(false) // estado para saber si esta fijo
 
   useEffect(function () {
     window.fetch('https://petgram-server-christian-christianvilca.vercel.app/categories')
@@ -13,8 +14,19 @@ export const ListOfCategories = () => {
       })
   }, [])
 
-  return (
-    <List>
+  useEffect(function () { // se ejecuta la funcion cada vez que se renderiza el componente
+    const onScroll = e => { // recive el evento del scroll
+      const newShowFixed = window.scrollY > 200 // mostrar las categorias fijas cuando el scroll verical sea mayor a 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed) // que ocurra solo cuando ya no esta fijo, y evitar volver a setear el estado cada vez que cambie el scroll
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map((category) => (
           <Item key={category.id}>
@@ -23,5 +35,12 @@ export const ListOfCategories = () => {
         ))
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
