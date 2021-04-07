d751ad0ea829bc62cbd6730ed3b0c94d9d920d8e
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-07 16:43:09 -0500

    4.5 - Uso de polyfill de Intersection Observer e imports dinamicos

--- src/Components/Photocard/index.js
+++ src/Components/Photocard/index.js
@@ -9,17 +9,25 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
 
   useEffect(function () { // si cada elemento esta en el viewport del usuario
-    // en la funcion se obtiene todas las entradas que estan en el viewport y esta observando
-    const observer = new window.IntersectionObserver(function (entries) {
-      const { isIntersecting } = entries[0] // nos ayuda a identificar si esta en el viewport
-      // console.log({ isIntersecting })
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect() // evitamos que el observador se vuelva a actualizar ya que solo queremos se actualize una vez
-      }
+    // lo queremos envolver en una promesa y cargar solo cuando lo necesitemos el polyfill
+    Promise.resolve(
+      // Chrome no necesita que cargue este polifill
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      // en la funcion se obtiene todas las entradas que estan en el viewport y esta observando
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0] // nos ayuda a identificar si esta en el viewport
+        // console.log({ isIntersecting })
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect() // evitamos que el observador se vuelva a actualizar ya que solo queremos se actualize una vez
+        }
+      })
+      // pasamos el elemento que queremos observar
+      observer.observe(ref.current)
     })
-    // pasamos el elemento que queremos observar
-    observer.observe(ref.current)
   }, [ref])
 
   // if (!show) return null // Si usamos esto nos dice que necesariamente tenemos que pasa el elemeto que tiene el ref
