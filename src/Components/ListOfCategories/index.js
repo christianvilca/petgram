9117443488689c6d060eaf8f42d12a0183d86c6a
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-06 22:07:55 -0500

    4.1 - Qué son los Hooks

--- src/Components/ListOfCategories/index.js
+++ src/Components/ListOfCategories/index.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]) // array vacio -> ya damos por hecho que es un array
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-christian-christianvilca.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => { // respuesta con las categorias
+        setCategories(response) // la respuesta se guarda en categories
+      })
+  }, [])
+
   return (
     <List>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))}
+      {
+        categories.map((category) => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      }
     </List>
   )
 }
