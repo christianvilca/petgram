b0991f7f3e66852dd6a8f7f790436254922acd9b
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-06 19:42:04 -0500

    3.7 - Creando animaciones con keyframes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ListOfCategories } from './Components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './Components/ListOfPhotoCards'
 import { Logo } from './Components/Logo'
 
