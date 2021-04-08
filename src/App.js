2b67e616086ed07cd7d29b03d8211afb368e30ae
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-07 23:40:16 -0500

    5.3 - Usar render Props para recuperar una foto

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import { ListOfCategories } from './Components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './Components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search) // Query string de la barra de direcciones
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : (
+            <>
+              <ListOfCategories />
+              <ListOfPhotoCards categoryId={2} />
+            </>
+            )
+      }
+    </div>
+  )
+}
