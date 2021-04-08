ab9a9e3d484650b947ba564b57c634eb97012b6d
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-08 16:03:40 -0500

    6.6 - Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import { Home } from './pages/Home'
 
 import { Router } from '@reach/router'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -19,6 +26,26 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+                )
+              : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+                )
+          }
+      </UserLogged>
+
       <NavBar />
     </div>
   )
