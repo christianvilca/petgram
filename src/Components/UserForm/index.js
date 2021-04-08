49e9191695847432fd8cfae30c77e45cfcf02294
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-08 17:05:19 -0500

    7.2.1 - Creación del componente UserForm;

---
+++ src/Components/UserForm/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export const UserForm = ({ onSubmit }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
+      <input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+      <button>Iniciar sesión</button>
+    </form>
+  )
+}
