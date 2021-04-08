bd369342648b41d5207c758a18aac2bb15f7135d
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-08 17:25:58 -0500

    7.2.2 -  Hook useInputValue

--- src/Components/UserForm/index.js
+++ src/Components/UserForm/index.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useInputValue } from '../../hooks/useInputValue'
 
 export const UserForm = ({ onSubmit }) => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const email = useInputValue('')
+  const password = useInputValue('')
 
   return (
     <form onSubmit={onSubmit}>
-      <input placeholder='Email' value={email} onChange={e => setEmail(e.target.value)} />
-      <input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+      <input placeholder='Email' {...email} />
+      <input placeholder='Password' {...password} />
       <button>Iniciar sesión</button>
     </form>
   )
