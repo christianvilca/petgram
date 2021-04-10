9a8023428834edf014fecda8ab99b98c63d34e0f
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-10 10:30:54 -0500

    8.1.1 - Redireccion a inicio sesion para acceder a ciertas rutas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './Components/Logo'
@@ -6,15 +6,16 @@ import { NavBar } from './Components/NavBar'
 
 import { Home } from './pages/Home'
 
-import { Router } from '@reach/router'
+import { Redirect, Router } from '@reach/router'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
-import Context from './Context'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
@@ -23,27 +24,13 @@ export const App = () => {
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? (
-                <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-                )
-              : (
-                <Router>
-                  <NotRegisteredUser path='/favs' />
-                  <NotRegisteredUser path='/user' />
-                </Router>
-                )
-          }
-      </Context.Consumer>
-
       <NavBar />
     </div>
   )
