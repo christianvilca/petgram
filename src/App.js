28f3b7632643ccfeb47c557ebaa143b3e9849c32
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-14 21:53:37 -0500

    8.4 - React.lazy() y componente Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './Components/Logo'
@@ -8,17 +8,19 @@ import { Home } from './pages/Home'
 
 import { Redirect, Router } from '@reach/router'
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NotFound } from './pages/NotFound'
 
 import { Context } from './Context'
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <div>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -35,6 +37,6 @@ export const App = () => {
         <User path='/user' />
       </Router>
       <NavBar />
-    </div>
+    </Suspense>
   )
 }
