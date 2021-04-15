f7308292aa1ac9738ffc0d1692daaa6e2699a49e
Christian Vilca <christian.vilcaq@gmail.com>
2021-04-14 23:09:27 -0500

    8.6 - PWA: generando el manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
 
 // la configuracion de webpack tiene que exportar un objeto
 module.exports = {
@@ -12,7 +14,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    }) // Asi utilizarmos la instacia del plugin
+    }), // Asi utilizarmos la instacia del plugin
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      short_name: 'Petgram',
+      description: 'Con Petgram puedes encontrar fotos de animales domesticos muy facilmente',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
+    })
   ],
   module: {
     rules: [ // reglas que tendran los modulos
